8e02286decd3fee9eb8213bf42badb3f8e754308
MA Jian <majian1979@qq.com>
2025-10-29 01:16:39 +0800

    Add files via upload

--- tch.r
+++ tch.r
@@ -21,7 +21,24 @@ tGumbelCopula = function(x){
   copent(x) - mean(dCopula(u,gCopula,log = TRUE))
 }
 
-tgc1a = tgc1b = 0
+tFrankCopula = function(x){
+  u = construct_empirical_copula(x) - 0.5 / dim(x)[1]
+  fit.tau = fitCopula(frankCopula(), u, method = "mpl")
+  tau = fit.tau@estimate
+  gCopula = frankCopula(param=tau)
+  copent(x) - mean(dCopula(u,gCopula,log = TRUE))
+}
+
+tClaytonCopula = function(x){
+  u = construct_empirical_copula(x) - 0.5 / dim(x)[1]
+  fit.tau = fitCopula(claytonCopula(), u, method = "mpl")
+  tau = fit.tau@estimate
+  gCopula = claytonCopula(param=tau)
+  copent(x) - mean(dCopula(u,gCopula,log = TRUE))
+}
+
+# simulation for testing Gaussian copula
+tgc1a = tgc1b = tgc1c = tgc1d = 0
 for(i in 1:9){
   mu = c(0,0)
   rho = i * 0.1
@@ -30,32 +47,47 @@ for(i in 1:9){
   x1 = rmvnorm(n,mu,sigma)
   tgc1a[i] = tGaussianCopula(x1)
   tgc1b[i] = tGumbelCopula(x1)
+  tgc1c[i] = tFrankCopula(x1)
+  tgc1d[i] = tClaytonCopula(x1)
 }
 
 x11()
 xlab1 = TeX(r'($\rho$)') 
 rho1 = seq(0.1,0.9,by = 0.1)
-plot(rho1,tgc1a, xlab = xlab1, ylab = 'statistic', ylim = c(min(tgc1a,tgc1b),max(tgc1a,tgc1b)), col = 'red', pch = 1);
+plot(rho1,tgc1a, xlab = xlab1, ylab = 'statistic', main = "Gaussian", ylim = c(min(tgc1a,tgc1b,tgc1c,tgc1d),max(tgc1a,tgc1b,tgc1c,tgc1d)), col = 'red', pch = 1);
 lines(rho1,tgc1a, col = 'red')
 points(rho1,tgc1b, col = 'blue', pch = 2); 
 lines(rho1,tgc1b, col = 'blue')
-legend(0.1,max(tgc1a,tgc1b),legend = c("Gaussian","Gumbel"), col = c("red","blue"), pch = 1:2)
+points(rho1,tgc1c, col = 'green', pch = 3); 
+lines(rho1,tgc1c, col = 'green')
+points(rho1,tgc1d, col = 'black', pch = 4); 
+lines(rho1,tgc1d, col = 'black')
+legend(0.1,max(tgc1a,tgc1b,tgc1d,tgc1d),legend = c("Gaussian","Gumbel","Frank","Clayton"), col = c("red","blue","green","black"), pch = 1:4)
 
-tgc2a = tgc2b = 0
+# simulation for testing Archimedean copula
+tgc2a = tgc2b = tgc2c = tgc2d = 0
 for(t in 2:10){
-  cop2 = gumbelCopula(t)
+  #cop2 = gumbelCopula(t); main1 = "Gumbel"
+  #cop2 = frankCopula(t); main1 = "Frank"
+  cop2 = claytonCopula(t); main1 = "Clayton"
   mvdc2 <- mvdc(cop2, c("norm", "exp"), list(list(mean = 0, sd =2), list(rate = 2)))
   n = 300
   x2 = rMvdc(n,mvdc2)
   tgc2a[t-1] = tGaussianCopula(x2)
   tgc2b[t-1] = tGumbelCopula(x2)
+  tgc2c[t-1] = tFrankCopula(x2)
+  tgc2d[t-1] = tClaytonCopula(x2)
 }
 
 x11()
 xlab2 = TeX(r'($\alpha$)') 
 alpha1 = 2:10
-plot(alpha1,tgc2a, xlab = xlab2, ylab = 'statistic', ylim = c(min(tgc2a,tgc2b),max(tgc2a,tgc2b)), col = 'red', pch =1);
+plot(alpha1,tgc2a, xlab = xlab2, ylab = 'statistic', main = main1, ylim = c(min(tgc2a,tgc2b,tgc2c,tgc2d),max(tgc2a,tgc2b,tgc2c,tgc2d)), col = 'red', pch =1);
 lines(alpha1,tgc2a, col = 'red')
 points(alpha1,tgc2b, col = 'blue', pch = 2); 
 lines(alpha1,tgc2b, col = 'blue')
-legend(2,max(tgc2a,tgc2b),legend = c("Gaussian","Gumbel"), col = c("red","blue"), pch = 1:2)
+points(alpha1,tgc2c, col = 'green', pch = 3); 
+lines(alpha1,tgc2c, col = 'green')
+points(alpha1,tgc2d, col = 'black', pch = 4); 
+lines(alpha1,tgc2d, col = 'black')
+legend(2,max(tgc2a,tgc2b,tgc2c,tgc2d),legend = c("Gaussian","Gumbel","Frank","Clayton"), col = c("red","blue","green","black"), pch = 1:4)
